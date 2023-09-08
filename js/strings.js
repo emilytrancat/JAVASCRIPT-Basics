042115fac61f0b3bdafb6bccae0a22e38d903227
emilytrancat <123094758+emilytrancat@users.noreply.github.com>
2023-09-08 17:09:35 -0500

    Introducing Strings

---
+++ js/strings.js
@@ -0,0 +1,13 @@
+// const message = "I'm a goofy goober.";
+// console.log(message);
+
+// const htmlSnippet = '<h1 class= "headline">My Headline</h1>';
+// console.log(htmlSnippet);
+
+const message = 'I\'m a goofy goober.';
+console.log(message);
+
+const multiline = "Hello, students. \
+Welcome to Javascript Basics. \
+I hope you learn a lot!";
+console.log(multiline);
