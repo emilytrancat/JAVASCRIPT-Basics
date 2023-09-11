434242d3d2a5f44e8bb703cb3c166bb829e5f3f9
emilytrancat <123094758+emilytrancat@users.noreply.github.com>
2023-09-11 16:38:38 -0500

    Change 12
    Display the Value of String on a Page

--- js/shout.js
+++ js/shout.js
@@ -2,4 +2,4 @@ const stringToShout = prompt("What do you want to shout?");
 const shout = stringToShout.toUpperCase();
 const shoutMessage = `<h2>The message to shout is: ${shout}!!!</h2>`;
 
-document.querySelector('main').innerHTML = shoutmessage;
+document.querySelector('main').innerHTML = shoutMessage;
