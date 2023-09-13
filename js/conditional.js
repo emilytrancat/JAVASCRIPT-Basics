512328cd1f5b3da156d7e613ee6dd064ce6ff7b1
emilytrancat <123094758+emilytrancat@users.noreply.github.com>
2023-09-13 15:01:18 -0500

    Change 15
    Introducing Conditional Statements

---
+++ js/conditional.js
@@ -0,0 +1,6 @@
+const answer = prompt('Which planet is closest to the sun?');
+if ( answer.toLowerCase() === 'mercury') {
+    console.log("That's correct!");
+} else { 
+    console.log("Sorry, that's incorrect!");
+}
