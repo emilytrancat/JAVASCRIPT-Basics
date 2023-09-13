561fbcd8eb149c148ea567f26afb85e1e0f98c1c
emilytrancat <123094758+emilytrancat@users.noreply.github.com>
2023-09-13 17:05:05 -0500

    Change 17
    Program Multiple Outcomes

---
+++ js/else-if.js
@@ -0,0 +1,17 @@
+const weather = '';
+
+// can change the value to either 'sun', 'rain', 'snow', or any other value (which will equal to the last "else" declared.)
+
+if ( weather === 'sun') {
+    console.log("It's sunny, so I'm going swimming.");
+
+} else if ( weather === 'rain') {
+    console.log("It's raining, so i will read a book.");
+
+}  else if ( weather === 'snow') {
+    console.log("It's snowing, so I'm going skiing.");
+
+} else { 
+    console.log("I'm stinky like poopoo.");
+
+}
